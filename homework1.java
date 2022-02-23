94bddb874b02a198b6ac327cd446fc58e529e7e2
ChoAnLee <97091064+ChoAnLee@users.noreply.github.com>
2022-02-24 00:35:04 +0800

    Create homework1.java

---
+++ homework1.java
@@ -0,0 +1,10 @@
+
+public class Extra_EXE_L1 {
+    public static void main(String [] args){
+        System.out.println("number  square  cube");
+        for(int i=0;i<11;i++){
+          System.out.printf("%d\t%d\t%d\n",i,i*i,i*i*i);  
+        }
+        
+    }
+}
